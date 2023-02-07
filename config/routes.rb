d9ca71fd19aff9f3b4cf26de2563158963ed3854
Iskander Hughes <iskanderhughes@hotmail.com>
2023-02-07 21:31:52 +0000

    began form routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :video_games do
     resources :video_games, only: %i[index new create show]
   end
+  resources :requests do
+    resources :requests, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
