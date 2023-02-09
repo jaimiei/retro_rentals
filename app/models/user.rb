c6257429b50b2a60d609b992f0c46c0ad9e1ed5a
Jaimie Irvine <jaimie.irvine1990@gmail.com>
2023-02-09 19:41:41 +0000

    added view for home page and added navbar with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  # validates :username, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   resources :video_games do
     resources :video_games, only: %i[index new create show]
