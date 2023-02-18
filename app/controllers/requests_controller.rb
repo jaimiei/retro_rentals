27afefff931b8815072e38c75cdc14e14ae369c0
Iskander Hughes <iskanderhughes@hotmail.com>
2023-02-18 15:06:30 +0000

    fixed merge clash

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,5 @@
 class RequestsController < ApplicationController
+  before_action :set_video_game, only: %i[create new show]
   def index
     @requests = current_user.requests
   end
@@ -8,20 +9,25 @@ class RequestsController < ApplicationController
   end
 
   def show
-    @request = Request.find(params[:id])
+    # @request = Request.find(params[:id])
   end
 
   def create
     @request = Request.new(request_params)
     @request.user = current_user
+    @request.video_game = @video_game
     if @request.save
-      redirect_to request_path(@request), notice: "Booking successful!"
+      redirect_to video_game_request_path(@video_game, @request), notice: "Booking successful!"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_video_game
+    @video_game = VideoGame.find(params[:video_game_id])
+  end
 
   def request_params
     params.require(:request).permit(:start_date, :end_date, :video_game_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :requests, only: [:index, :new, :create, :show]
+  resources :requests, only: [:index]
   resources :video_games, only: [:index, :new, :create, :show] do
     resources :reviews, only: [:new, :create]
     resources :requests, only: [:new, :create, :show]
